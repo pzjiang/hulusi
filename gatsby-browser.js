47da6109099a11f2966048d693a4687656e68769
pzjiang <pzjiang01@hotmail.com>
2021-06-14 15:37:00 -0400

    importing styles

---
+++ gatsby-browser.js
@@ -0,0 +1,5 @@
+import "./src/styles/global.css"
+import "./src/styles/layoutstyle.css"
+import "./src/styles/groupstyle.css"
+import "./src/styles/contactstyle.css"
+import "./src/styles/syllabusstyle.css"
